39b688e33b988743da038ab3d2b80f4f21185a64
NiluK <nkulasingham@gmail.com>
2015-12-17 15:48:31 +1300

    simplified it into a single function

--- app/twitter.js
+++ app/twitter.js
@@ -3,32 +3,34 @@ const dotenv = require('dotenv')
 const Promise = require('bluebird')
 const R = require('ramda')
 const Twitter = require('twitter')
+
 dotenv.load()
 
-const client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-})
+const postTweet = (tweettext) => {
+  const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  })
+  const twitter = Promise.promisifyAll(client)
+  twitter.post('statuses/update', {status: tweettext}, (error, tweet, response) => {
+    if (error) {
+      console.log(error)
+      return tweet
+    } else {
+      console.log(tweet)
+      return tweet
+    }
+  })
+  twitter.getAsync('statuses/user_timeline', {screen_name: 'avianesc'})
+  .then(twitterResponse => {
+    const getText = status => {
+      return status.text
+    }
+    let tweets = R.map(getText, twitterResponse)
+    console.log(tweets)
+  })
+}
 
-const twitter = Promise.promisifyAll(client)
-
-const twitterResponse = twitter.getAsync('statuses/user_timeline', {screen_name: 'avianesc'})
-twitterResponse
-.then(twitterResponse => {
-  const getText = status => {
-    return status.text
-  }
-  let tweets = R.map(getText, twitterResponse)
-  console.log(tweets)
-})
-.then(client.post('statuses/update', {status: 'hello'}, (error, tweet, response) => {
-  if (error) {
-    console.log(error)
-    return tweet
-  } else {
-    console.log(tweet)
-    return tweet
-  }
-}))
+module.exports = postTweet
