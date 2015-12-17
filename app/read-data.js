5b4478f63fbc94b8c6f90d8f6c5179dfe9124f08
NiluK <nkulasingham@gmail.com>
2015-12-18 11:44:36 +1300

    installed superagent

--- app/read-data.js
+++ app/read-data.js
@@ -6,7 +6,7 @@ const today = new Date()
 const currentDate = today.getUTCFullYear() + "-" + (today.getUTCMonth() + 1) + "-" + (today.getUTCDate() - 1)
 
 
-new CronJob('* /10 * * * *', function() {
+new CronJob('*/10 * * * *', function() {
   request
   .get('http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=' + currentDate )
   .end((err, res) => {
