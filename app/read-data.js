63b0dbd31dde5f4f041a5bd9b280ceffd3b21162
colin boyd <email": null>
2015-12-17 15:27:02 +1300

    getting usgs data in array

--- app/read-data.js
+++ app/read-data.js
@@ -2,42 +2,21 @@
 
 const request = require('superagent')
 
-
-var array = []
-
-
-// const usgs = (url, callback) => {
-//   request(url)
-//     .end(callback)
-// }
-
 request
-  .get('http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2015-12-15')
+  .get('http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2015-12-16')
   .end((err, res) => {
-    var quakeData
+    var array = []
     for ( var i = 0; i<res.body.features.length; i++) {
-
+      array.push({
+        time: (res.body.features[i].properties.time),
+        url: (res.body.features[i].properties.url),
+        mag: (res.body.features[i].properties.mag),
+        place: (res.body.features[i].properties.place),
+        coordinates: (res.body.features[i].geometry.coordinates)
+      })
     }
-    quakeData.url = (res.body.features[1].properties.url)
-    quakeData.time = (res.body.features[1].properties.time)
-    quakeData.mag = (res.body.features[1].properties.mag)
-    quakeData.place = (res.body.features[1].properties.place)
-    quakeData.coordinates = (res.body.features[1].geometry.coordinates)
-    console.log(quakeData)
-
-
-
-
-
-
-
-
-
-
-})
-
-
-
+    console.log(array)
+  })
 
 
 // 10 minutes interval between calling website.
@@ -46,6 +25,6 @@ request
 // filter by magnitude & location
 // persistence of pull it. make sure it's not the same data as before.
 // push to el twitter
-//
-
+        // if quakeData.time of this index ocurred within 10 minutes of this time\
+        // then add the data to the object
 
