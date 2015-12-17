03949ea6bbe73b259e4f81eacedd0c46cbf18287
colin boyd <email": null>
2015-12-17 18:44:00 +1300

    request usgs earthquake data is working

--- app/read-data.js
+++ app/read-data.js
@@ -2,22 +2,33 @@
 
 const request = require('superagent')
 
+const today = new Date()
+const currentDate = today.getUTCFullYear() + "-" + (today.getUTCMonth() + 1) + "-" + (today.getUTCDate() - 1)
+
 request
-  .get('http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2015-12-16')
+  .get('http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=' + currentDate )
   .end((err, res) => {
     var earthquakeArray = []
+    const currentTime =  Math.floor(Date.now() / 1000)
+
     for ( var i = 0; i<res.body.features.length; i++) {
-      if ( res.body.features[i].properties.mag > 4) {
-        earthquakeArray.push({
-        time: (res.body.features[i].properties.time),
-        url: (res.body.features[i].properties.url),
-        mag: (res.body.features[i].properties.mag),
-        place: (res.body.features[i].properties.place),
-        coordinates: (res.body.features[i].geometry.coordinates)
-      })
+       let earthquakeTime = res.body.features[i].properties.time /1000
+       let timeDiff = currentTime - earthquakeTime
+
+      if ( res.body.features[i].properties.mag > 4 && timeDiff < 600 ) {
+          earthquakeArray.push({
+          time: (res.body.features[i].properties.time),
+          url: (res.body.features[i].properties.url),
+          mag: (res.body.features[i].properties.mag),
+          place: (res.body.features[i].properties.place),
+          coordinates: (res.body.features[i].geometry.coordinates)
+        })
+          console.log(timeDiff)
+          console.log(earthquakeArray)
       }
-    }
-    console.log(earthquakeArray)
+      }
+
+    // console.log(earthquakeArray)
   })
 
 
@@ -29,4 +40,3 @@ request
 // push to el twitter
         // if quakeData.time of this index ocurred within 10 minutes of this time\
         // then add the data to the object
-
