a7bebf5680ca8b53e13bc27cc56b1509cf6492a0
NiluK <nkulasingham@gmail.com>
2015-12-18 14:10:27 +1300

    pushing to master because I only changed one thing

--- app/twitter.js
+++ app/twitter.js
@@ -1,6 +1,7 @@
 'use strict'
 const dotenv = require('dotenv')
 const Promise = require('bluebird')
+const R = require('ramda')
 const Twitter = require('twitter')
 
 dotenv.load()
@@ -18,9 +19,17 @@ const postTweet = (tweettext) => {
       console.log(error)
       return tweet
     } else {
-      console.log(tweet.text)
-      return tweet.text
+      console.log(tweet)
+      return tweet
     }
+  })
+  twitter.getAsync('statuses/user_timeline', {screen_name: 'avianesc'})
+  .then(twitterResponse => {
+    const getText = status => {
+      return status.text
+    }
+    let tweets = R.map(getText, twitterResponse)
+    console.log(tweets)
   })
 }
 
